9b92e098d996102136f95fa3dd5e3717b4c85532
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-04-16 07:53:03 +0530

    updating version of project cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
