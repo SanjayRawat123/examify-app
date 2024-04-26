7959a01faa70519b834db698f243d616f29724d7
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-04-26 07:52:23 +0530

    chnaged the logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
+    
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
