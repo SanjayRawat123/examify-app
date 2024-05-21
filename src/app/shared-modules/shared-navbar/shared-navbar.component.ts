31ecf1ed2f3e891a7e2f7ae1f4b9430daabbcfd4
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-05-21 07:57:35 +0530

    changes made in shared navbar

--- src/app/shared-modules/shared-navbar/shared-navbar.component.ts
+++ src/app/shared-modules/shared-navbar/shared-navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/backend-services/user-service/user.service';
 import { SidebarCollapseService } from 'src/app/ui-services/side-bar-service/sidenar-collapse.service';
+import { ThemeService } from 'src/app/ui-services/theme.service';
 import { Data } from 'src/types/examify-interface';
 
 @Component({
@@ -11,11 +12,13 @@ import { Data } from 'src/types/examify-interface';
 })
 export class SharedNavbarComponent implements OnInit {
   isSidebarExpanded: boolean = true;
+  isDarkMode: boolean = false;
   user!: Data.User;
   constructor(
     private sidebarService: SidebarCollapseService,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    public themeService: ThemeService
   ) {
     this.sidebarService.isExpanded$.subscribe((isExpanded: boolean) => {
       console.log(isExpanded);
@@ -23,12 +26,18 @@ export class SharedNavbarComponent implements OnInit {
     });
   }
   ngOnInit(): void {
+    this.isDarkMode = this.themeService.getDarkTheme;
     this.user = this.userService.getUser();
     console.log(this.user);
+  }
+  
+  toggleTheme() {
+    this.themeService.toggleTheme();
+    this.isDarkMode = this.themeService.getDarkTheme;
   }
 
   public logout() {
     this.userService.logout();
-    this.router.navigate(['/home']);
+    this.router.navigate(['home']);
   }
 }
