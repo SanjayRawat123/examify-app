4f90dac20257cbb20903b5672b978698126d8b84
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-06-19 08:21:17 +0530

    Generated new component to view questions of quizzes

---
+++ src/app/admin/components/view-quiz-questions/view-quiz-questions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-quiz-questions',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-quiz-questions.component.html',
+  styleUrl: './view-quiz-questions.component.scss'
+})
+export class ViewQuizQuestionsComponent {
+
+}
